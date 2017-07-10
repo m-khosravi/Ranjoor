1ee14ead67367e4c7127c464b47c12c37eb1e041
aligholamee <aligholami7596@gmail.com>
2017-07-10 13:36:16 +0430

    remove FadeInAnimation - add RNANIMATABLE

--- src/Pages/RanjoorMain.js
+++ src/Pages/RanjoorMain.js
@@ -11,11 +11,9 @@ class RanjoorMain extends Component {
     render() {
         return(
             <View>
-
+                <Text>This is the Ranjoor's main page</Text>
             </View>
         );
     }
 }
-
-
 export default RanjoorMain
