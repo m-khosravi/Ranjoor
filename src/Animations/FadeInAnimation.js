0e34e7dbf0bb57ec0095a33c96816c867ffaa9ab
aligholamee <aligholami7596@gmail.com>
2017-07-10 12:45:15 +0430

    simply call the navigate when the animation is completed

--- src/Animations/FadeInAnimation.js
+++ src/Animations/FadeInAnimation.js
@@ -1,35 +1,37 @@
 import React from 'react';
 import { Animated, Text, View } from 'react-native';
+import SplashScreen from '../Pages/SplashScreen';
+import { StackNavigator } from 'react-navigation';
 
 class FadeInView extends React.Component {
-  state = {
+state = {
     fadeAnim: new Animated.Value(0),  // Initial value for opacity: 0
-  }
+}
 
-  componentDidMount() {
+componentDidMount() {
     Animated.timing(                  // Animate over time
-      this.state.fadeAnim,            // The animated value to drive
-      {
+    this.state.fadeAnim,            // The animated value to drive
+    {
         toValue: 1,                   // Animate to opacity: 1 (opaque)
         duration: 1000,              // Make it take a while
-      }
-    ).start();                        // Starts the animation
-  }
+    }
+    ).start(() => SplashScreen.props.navigation.navigate('Main'));                        // Starts the animation
+}
 
-  render() {
+render() {
     let { fadeAnim } = this.state;
 
     return (
-      <Animated.View                 // Special animatable View
+    <Animated.View                 // Special animatable View
         style={{
-          ...this.props.style,
-          opacity: fadeAnim,         // Bind opacity to animated value
+        ...this.props.style,
+        opacity: fadeAnim,         // Bind opacity to animated value
         }}
-      >
+    >
         {this.props.children}
-      </Animated.View>
+    </Animated.View>
     );
-  }
+}
 }
 
 export default FadeInView
