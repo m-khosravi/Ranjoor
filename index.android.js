f8e675f568d107d5b23d90e460e84e957bca4240
aligholamee <aligholami7596@gmail.com>
2017-07-10 10:14:45 +0430

    update colors

--- index.android.js
+++ index.android.js
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#253028',
   },
   welcome: {
     fontSize: 20,
