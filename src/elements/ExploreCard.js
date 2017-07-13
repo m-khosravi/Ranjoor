d09e3b30d27d49709dd23661ccf5b3dcb2344a25
aligholamee <aligholami7596@gmail.com>
2017-07-13 11:37:48 +0430

    zIndex is missing

--- src/elements/ExploreCard.js
+++ src/elements/ExploreCard.js
@@ -10,44 +10,47 @@ import { Card, ListItem, Button, Icon, Avatar} from 'react-native-elements';
 export default class ExploreCard extends React.Component {
     render() {
         return(
-            <View>
-                <Avatar
-                medium
-                rounded 
-                containerStyle={{backgroundColor:'red'}}
-                containerStyle={{alignSelf:'flex-end', marginRight:25, borderColor:'red'}}/>
-                <Card
-                containerStyle={{height: 150, backgroundColor:'#202026', shadowOpacity: 0.7,
-                                    shadowOffset:{height:5}, shadowColor: 'black', borderWidth: 0, borderRadius: 8, flexDirection: 'row'}}
-                wrapperStyle={{alignSelf:'flex-end'}} >
+                <View>
+                    <Card
+                    containerStyle={{height: 150, backgroundColor:'#202026', shadowOpacity: 0.7,
+                                        shadowOffset:{height:5}, shadowColor: 'black', borderWidth: 0, borderRadius: 8, flexDirection: 'row'}}
+                    wrapperStyle={{alignSelf:'flex-end'}} >
 
-                    <View style={{ flex:2,}}>
-                                <Text style={{alignSelf:'flex-end', fontFamily:'IRANSans', color:'white', marginTop: 10, marginRight:10}}>
-                                    ساقیا بده جامی زان شراب روحانی
-                                </Text>
-                        <Text style={{alignSelf:'flex-start', fontFamily:'IRANSans', color:'white'}}>
-                            تا دمی برآساییم زین حجاب ظلمانی
-                        </Text>
-                    </View>
-                    <View style={{alignSelf:'flex-end', backgroundColor:'transparent', flexDirection: 'row'}}>
-                        <Icon 
-                        name='favorite' size={24} color="#34343a" style={{marginLeft:5}}
-                        />                    
-                        <Icon 
-                        name='grade' size={24} color="#34343a" style={{marginLeft:5}}
-                        />           
-                        <Button
-                            textStyle={{fontSize:15}}
-                            iconRight
-                            backgroundColor='#4ab367'
-                            fontFamily='IRANSans_UltraLight'
-                            buttonStyle={{height: 15, width: 110, 
-                                borderRadius: 8, marginLeft: 165}}
-                            title='ادامه مطلب'
-                        />
-                    </View>
-                </Card>
-            </View>
+                        <View style={{ flex:2,}}>
+                                    <Text style={{alignSelf:'flex-end', fontFamily:'IRANSans', color:'white', marginTop: 10, marginRight:10}}>
+                                        ساقیا بده جامی زان شراب روحانی
+                                    </Text>
+                            <Text style={{alignSelf:'flex-start', fontFamily:'IRANSans', color:'white'}}>
+                                تا دمی برآساییم زین حجاب ظلمانی
+                            </Text>
+                        </View>
+                        <View style={{alignSelf:'flex-end', backgroundColor:'transparent', flexDirection: 'row'}}>
+                            <Icon 
+                            name='favorite' size={24} color="#34343a" style={{marginLeft:5}}
+                            />                    
+                            <Icon 
+                            name='grade' size={24} color="#34343a" style={{marginLeft:5}}
+                            />           
+                            <Button
+                                textStyle={{fontSize:15}}
+                                iconRight
+                                backgroundColor='#4ab367'
+                                fontFamily='IRANSans_UltraLight'
+                                buttonStyle={{height: 15, width: 110, 
+                                    borderRadius: 8, marginLeft: 165}}
+                                title='ادامه مطلب'
+                            />
+                        </View>
+                    </Card>
+                    <Avatar
+                    medium
+                    rounded 
+                    source={require('../img/avatars/ferdowsi.jpg')}
+                    containerStyle={{alignSelf:'flex-start', marginRight:25,
+                                    shadowOpacity: 0.7,
+                                    shadowOffset:{height:5}, shadowColor: 'black'}}
+                    />
+                </View>
         );
     }
 }
