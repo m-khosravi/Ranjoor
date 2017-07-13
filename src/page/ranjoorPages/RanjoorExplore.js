c30fbf3231737d13f1f1e81a3e79cd5957fbce36
aligholamee <aligholami7596@gmail.com>
2017-07-13 10:10:56 +0430

    Explore Cards improved

--- src/page/ranjoorPages/RanjoorExplore.js
+++ src/page/ranjoorPages/RanjoorExplore.js
@@ -28,20 +28,31 @@ class RanjoorExplore extends React.Component {
             <View style={styles.ExploreContainer}>
                 <Card
                 containerStyle={{height: 150, backgroundColor:'#202026', shadowOpacity: 0.7,
-                                 shadowOffset:{height:5}, shadowColor: 'black', borderWidth: 0, borderRadius: 8}}
-                >
-                <View style={{backgroundColor:'transparent'}}>
-                    <Button
-                        iconRight
-                        icon={{name: 'book'}}
-                        backgroundColor='#4ab367'
-                        fontFamily='IRANSans'
-                        buttonStyle={{alignSelf: 'flex-end', height: 15, width: 100, 
-                            borderRadius: 8, marginLeft: 0, 
-                            marginRight: 0, marginBottom: 0}}
-                        title='ادامه شعر'
-                    />                
-                </View>
+                                 shadowOffset:{height:5}, shadowColor: 'black', borderWidth: 0, borderRadius: 8, flexDirection: 'row'}}
+                wrapperStyle={{alignSelf:'flex-end'}} >
+                    <View style={{ flex:2,}}>
+                                <Text style={{alignSelf:'flex-end', fontFamily:'IRANSans', color:'white', marginTop: 10, marginRight:10}}>
+                                    ساقیا بده جامی زان شراب روحانی
+                                </Text>
+                        <Text style={{alignSelf:'flex-start', fontFamily:'IRANSans', color:'white', marginLeft:10}}>
+                            تا دمی برآساییم زین حجاب ظلمانی
+                        </Text>
+                    </View>
+                    <View style={{alignSelf:'flex-end', backgroundColor:'transparent', flexDirection: 'row'}}>
+                        <Icon name="linkedin-square" size={20} color="#34343a" style={{ marginLeft: 10 }}/>
+                        <Icon name="telegram" size={20} color="#34343a" style={{ marginLeft: 8 }}/>
+                        <Icon name="instagram" size={20} color="#34343a" style={{ marginLeft: 8 }}/>                    
+                        <Button
+                            iconRight
+                            icon={{name: 'book'}}
+                            backgroundColor='#4ab367'
+                            fontFamily='IRANSans'
+                            buttonStyle={{height: 15, width: 100, 
+                                borderRadius: 8, marginLeft: 150, 
+                                marginRight: 0, marginBottom: 0}}
+                            title='ادامه شعر'
+                        />
+                    </View>
                 </Card>
             </View>
         );
