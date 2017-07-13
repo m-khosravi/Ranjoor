9484e0fbb7670f48ac642dccb8307f5ebdd0ab88
aligholamee <aligholami7596@gmail.com>
2017-07-13 12:22:23 +0430

    Improve card layout

--- src/elements/ExploreCard.js
+++ src/elements/ExploreCard.js
@@ -10,14 +10,25 @@ import { Card, ListItem, Button, Icon, Avatar} from 'react-native-elements';
 export default class ExploreCard extends React.Component {
     render() {
         return(
-                <View>
-                    <Card
+                 <Card
                     containerStyle={{height: 150, backgroundColor:'#202026', shadowOpacity: 0.7,
                                         shadowOffset:{height:5}, shadowColor: 'black', borderWidth: 0, borderRadius: 8, flexDirection: 'row'}}
                     wrapperStyle={{alignSelf:'flex-end'}} >
 
-                        <View style={{ flex:2,}}>
-                                    <Text style={{alignSelf:'flex-end', fontFamily:'IRANSans', color:'white', marginTop: 10, marginRight:10}}>
+                        <View style={{ flex:2,}}>  
+                            <View style={{flexDirection:'row',alignSelf:'flex-end'}}>
+                                    <Text style={{fontFamily:'IRANSans', marginRight:5, marginTop:8, color:'#505056'}}>حافظ</Text> 
+                                    <Avatar
+                                    small
+                                    rounded 
+                                    source={require('../img/avatars/ferdowsi.jpg')}
+                                    containerStyle={{alignSelf:'flex-end', marginRight:15,
+                                                    shadowOpacity: 0.7,
+                                                    shadowOffset:{height:5}, shadowColor: 'black'}}
+                                    />  
+                            </View>
+                   
+                                    <Text style={{alignSelf:'flex-end', fontFamily:'IRANSans', color:'white', marginTop: 10, marginRight:15}}>
                                         ساقیا بده جامی زان شراب روحانی
                                     </Text>
                             <Text style={{alignSelf:'flex-start', fontFamily:'IRANSans', color:'white'}}>
@@ -42,15 +53,6 @@ export default class ExploreCard extends React.Component {
                             />
                         </View>
                     </Card>
-                    <Avatar
-                    medium
-                    rounded 
-                    source={require('../img/avatars/ferdowsi.jpg')}
-                    containerStyle={{alignSelf:'flex-start', marginRight:25,
-                                    shadowOpacity: 0.7,
-                                    shadowOffset:{height:5}, shadowColor: 'black'}}
-                    />
-                </View>
         );
     }
 }
