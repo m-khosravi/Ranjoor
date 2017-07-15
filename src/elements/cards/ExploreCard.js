31d959083bccb5f6eea5d289042a30183c9ccf6e
aligholamee <aligholami7596@gmail.com>
2017-07-15 20:04:16 +0430

    solved :)

--- src/elements/cards/ExploreCard.js
+++ src/elements/cards/ExploreCard.js
@@ -11,7 +11,7 @@ import { Card, ListItem, Button, Icon, Avatar, Grid, Row, Col } from 'react-nati
 export default class ExploreCard extends Component {
     render() {
         /* Mapped data will be processed right here */
-        let mappedData = this.props.data.map(function (data1) {
+        let mappedData = this.props.data.map(function (data1, index) {
             return (
                 <View style={{flexDirection:'row'}}>
                     <View style={{ flex: 1 }}></View>
@@ -19,11 +19,11 @@ export default class ExploreCard extends Component {
                         containerStyle={{
                              height: 250, backgroundColor: '#202026', shadowOpacity: 0.7,
                             shadowOffset: { height: 5 }, shadowColor: 'black', borderWidth: 0, borderRadius: 8, flexDirection: 'row'}}
-                        wrapperStyle={{width:'100%'}}                            
+                        wrapperStyle={{width:'100%', flexDirection:'column'}}                            
                             >
                         <Grid>
-                            <Row style={{justifyContent:'flex-start'}}>
-                                <View style={{ flexDirection: 'row', alignSelf: 'flex-start' }}>
+                            <Row style={{flexDirection:'row', justifyContent:'flex-end'}}>
+                                <View style={{ flexDirection: 'row', alignSelf: 'flex-end' }}>
                                     <Text style={{ fontFamily: 'IRANSans', marginRight: 5, marginTop: 12, color: '#505056' }}>حافظ</Text>
                                     <Avatar
                                         medium
@@ -61,7 +61,7 @@ export default class ExploreCard extends Component {
                                         backgroundColor='#4ab367'
                                         fontFamily='IRANSans_UltraLight'
                                         buttonStyle={{
-                                            height: 15, width: 110,
+                                            height: 15, width: 100,
                                             borderRadius: 8
                                         }}
                                         title='ادامه مطلب'
