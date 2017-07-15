9394084b10cf4c2d7bc9eb8da3bc1dc558c0ec66
aligholamee <aligholami7596@gmail.com>
2017-07-15 18:41:37 +0430

    improve the UI stability with GRID layout

--- src/elements/cards/ExploreCard.js
+++ src/elements/cards/ExploreCard.js
@@ -1,34 +1,28 @@
-        import React, { Component } from 'react';
-        import {
-            StyleSheet,
-            Text,
-            View,
-            Image,
-            Alert
-        } from 'react-native';
-        import { Card, ListItem, Button, Icon, Avatar } from 'react-native-elements';
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Alert
+} from 'react-native';
+import { Card, ListItem, Button, Icon, Avatar, Grid, Row, Col } from 'react-native-elements';
 
-        export default class ExploreCard extends Component {
-            render() {
-                /* Mapped data will be processed right here */
-                    let mappedData = this.props.data.map(function (data1) {
-                        return (
-                                data1.title
-                        )
-                    })
-
-                return (
-                    <View style={{ flexDirection: 'row' }}>
-                        <View style={{ flex: 1 }}></View>
-                        <Card
-                            containerStyle={{
-                                width: '85%', height: 250, backgroundColor: '#202026', shadowOpacity: 0.7,
-                                shadowOffset: { height: 5 }, shadowColor: 'black', borderWidth: 0, borderRadius: 8, flexDirection: 'row'
-                            }}
-                            wrapperStyle={{ alignSelf: 'flex-end' }} >
-
-                            <View style={{ flex: 2, alignSelf: 'flex-end' }}>
-                                <View style={{ flexDirection: 'row', alignSelf: 'flex-end' }}>
+export default class ExploreCard extends Component {
+    render() {
+        /* Mapped data will be processed right here */
+        let mappedData = this.props.data.map(function (data1) {
+            return (
+                <View style={{flexDirection:'row'}}>
+                    <View style={{ flex: 1 }}></View>
+                    <Card
+                        containerStyle={{
+                            width: '85%', height: 250, backgroundColor: '#202026', shadowOpacity: 0.7,
+                            shadowOffset: { height: 5 }, shadowColor: 'black', borderWidth: 0, borderRadius: 8, flexDirection: 'row'
+                        }}>
+                        <Grid>
+                            <Row>
+                                <View style={{ flexDirection: 'row', alignSelf: 'flex-start' }}>
                                     <Text style={{ fontFamily: 'IRANSans', marginRight: 5, marginTop: 12, color: '#505056' }}>حافظ</Text>
                                     <Avatar
                                         medium
@@ -41,24 +35,24 @@
                                         }}
                                     />
                                 </View>
-
+                            </Row>
+                            <Row>
                                 <View>
                                     <Text style={{ alignSelf: 'flex-end', fontFamily: 'IRANSans', color: 'white', marginTop: '10%', marginRight: '5%' }}>
-                                        {mappedData}
+                                        {data1.title}
                                     </Text>
-                                    <Text style={{ alignSelf: 'flex-start', fontFamily: 'IRANSans', color: 'white' }}>
-                                        تا دمی برآساییم زین حجاب ظلمانی
-                                </Text>
                                 </View>
-                            </View>
-                            <View style={{ alignSelf: 'flex-end', backgroundColor: 'transparent', flexDirection: 'row' }}>
-                                <Icon
-                                    name='favorite' size={24} color="#34343a" style={{ marginLeft: 5 }}
-                                />
-                                <Icon
-                                    name='grade' size={24} color="#34343a" style={{ marginLeft: 5 }}
-                                />
-                                <View>
+                            </Row>
+                            <Row style={{ justifyContent: 'space-between' }}>
+                                <Col style={{ flexDirection: 'row' }}>
+                                    <Icon
+                                        name='favorite' size={24} color="#34343a" style={{ marginLeft: 5 }}
+                                    />
+                                    <Icon
+                                        name='grade' size={24} color="#34343a" style={{ marginLeft: 5 }}
+                                    />
+                                </Col>
+                                <Col style={{ flexDirection: 'row' }}>
                                     <Button
                                         textStyle={{ fontSize: 15 }}
                                         iconRight
@@ -70,13 +64,18 @@
                                         }}
                                         title='ادامه مطلب'
                                     />
-
-                                </View>
-
-                            </View>
-                        </Card>
-                        <View style={{ flex: 1 }}></View>
-                    </View>
-                );
-            }
-        }
+                                </Col>
+                            </Row>
+                        </Grid>
+                    </Card>
+                    <View style={{ flex: 1 }}></View>
+                </View>
+            )
+        })
+        return (
+            <View>
+                {mappedData}
+            </View>
+        )
+    }
+}
