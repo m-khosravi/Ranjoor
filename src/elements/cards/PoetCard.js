d15ab5706662f2a38f0e6a7bbd928d55d9829778
aligholamee <aligholami7596@gmail.com>
2017-07-16 20:11:32 +0430

    animation was added on open/close

--- src/elements/cards/PoetCard.js
+++ src/elements/cards/PoetCard.js
@@ -9,25 +9,39 @@ import {
 import { Card, ListItem, Button, Avatar, Grid, Row, Col } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Accordion from 'react-native-collapsible/Accordion';
+import * as Animatable from 'react-native-animatable';
 
 
 class PoetCard extends React.Component {
 
-    _renderHeader(section) {
-        return (
-            <View >
-                <Text>{section.name}</Text>
-            </View>
-        );
-    }
+  _renderHeader(section, index, isActive) {
+    return (
+      <Animatable.View
+        style={styles.header}
+        duration={300}
+        transition="backgroundColor"
+        style={{ backgroundColor: (isActive ? 'rgba(255,255,255,1)' : 'rgba(245,252,255,1)') }}>
+        <Text style={styles.headerText}>{section.title}</Text>
+      </Animatable.View>
+    );
+  }
 
-    _renderContent(section) {
-        return (
-            <View>
-                <Text>{section.name}</Text>
-            </View>
-        );
-    }
+  _renderContent(section, i, isActive) {
+    return (
+      <Animatable.View
+        style={styles.content}
+        duration={300}
+        transition="backgroundColor"
+        style={{ backgroundColor: (isActive ? 'rgba(255,255,255,1)' : 'rgba(245,252,255,1)') }}>
+        <Animatable.Text
+          duration={300}
+          easing="ease-out"
+          animation={isActive ? 'zoomIn' : false}>
+          {section.content}
+        </Animatable.Text>
+      </Animatable.View>
+    );
+  }
 
     render() {
         /* Mapped data will be processed right here */
