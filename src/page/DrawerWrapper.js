d605f7c74358114bf56101df22b9c0b842d13781
aligholamee <aligholami7596@gmail.com>
2017-07-24 18:53:44 +0430

    Customized drawer was added

--- src/page/DrawerWrapper.js
+++ src/page/DrawerWrapper.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 import { TabNavigator, DrawerNavigator } from 'react-navigation';
 import TabWrapper from './RanjoorPagesWrapper';
+import MorphDrawer from '../elements/drawerMaterial/CustomizedDrawer';
 
 
 const DrawerWrapper = new DrawerNavigator({
