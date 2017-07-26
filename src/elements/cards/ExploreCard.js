d3eef0d47db7dbccad54adf780ae9018765b50f2
aligholamee <aligholami7596@gmail.com>
2017-07-26 17:03:29 +0430

    Progress styles was added

--- src/elements/cards/ExploreCard.js
+++ src/elements/cards/ExploreCard.js
@@ -119,3 +119,24 @@ export default class ExploreCard extends Component {
     }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingVertical: 20,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  circles: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  progress: {
+    margin: 10,
+  },
+});
