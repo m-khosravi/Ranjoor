12c3ac28aff2aa18c1b086a3e4557bd252f927ca
aligholamee <aligholami7596@gmail.com>
2017-07-31 19:57:43 +0430

    map the local state

--- src/page/ranjoorPages/RanjoorExplore.js
+++ src/page/ranjoorPages/RanjoorExplore.js
@@ -53,6 +53,10 @@ class RanjoorExplore extends Component {
         this.setState({ dataset })
     }
 
+    componentDidmount() {
+        this.setupImpagination()
+    }
+
 
     static navigationOptions = {
         header: null,
@@ -77,13 +81,80 @@ class RanjoorExplore extends Component {
             <View style={styles.ExploreContainer}>
                 <ExploreHeader />
                 <ScrollView >
-                    <StatusBar
-                        backgroundColor="#705697"
-                        barStyle="light-content"
-                    />
                     <View>
-                        <ExploreCard />
-                        <ExploreCard />
+                        {this.state.datasetState.map(record => {
+                            return (
+                                <Animatable.View animation="fadeIn" style={{ flexDirection: 'row', marginBottom: 2 }}>
+                                    <View style={{ flex: 1 }}></View>
+                                    <Card
+                                        containerStyle={{
+                                            marginBottom: 3,
+                                            height: 180, backgroundColor: '#fafafa', shadowOpacity: 0.7,
+                                            shadowOffset: { height: 5 }, shadowColor: 'black', borderWidth: 0, borderRadius: 5, flexDirection: 'row'
+                                        }}
+                                        wrapperStyle={{ width: '100%', flexDirection: 'column' }}
+                                    >
+                                        <Grid>
+                                            <Row style={{ flexDirection: 'row', justifyContent: 'flex-end', flex: 1 }}>
+                                                <View style={{ flexDirection: 'row', alignSelf: 'flex-end' }}>
+                                                    <Text style={{ fontFamily: 'IRANSans', marginRight: 5, marginTop: 12, color: '#505056' }}>{this.state.rawData.poet}</Text>
+                                                    <Avatar
+                                                        medium
+                                                        rounded
+                                                        source={require('../../img/avatars/ferdowsi.jpg')}
+                                                        containerStyle={{
+                                                            backgroundColor: '#705697',
+                                                            alignSelf: 'flex-start',
+                                                            shadowOpacity: 0.7,
+                                                            shadowOffset: { height: 5 }, shadowColor: 'black'
+                                                        }}
+                                                    />
+                                                </View>
+                                            </Row>
+                                            <Row style={{ flexDirection: 'row', alignSelf: 'flex-end', flex: 1 }}>
+                                                <View style={{ flex: 1, margin: 10 }}>
+                                                    <View style={{ alignSelf: 'flex-end', flex: 1 }}>
+                                                        <Text style={{ fontSize: 14, alignSelf: 'flex-end', fontFamily: 'IRANSans_Bold', color: 'black' }}>
+                                                            {this.state.rawData.m1}
+                                                        </Text>
+                                                    </View>
+                                                    <View style={{ alignSelf: 'flex-start', flex: 1 }}>
+                                                        <Text style={{ fontSize: 14, alignSelf: 'flex-start', fontFamily: 'IRANSans_Bold', color: 'black', marginTop: 10 }}>
+                                                            {this.state.rawData.m2}
+                                                        </Text>
+                                                    </View>
+                                                </View>
+                                            </Row>
+                                            <Row style={{ flexDirection: 'row', flex: 1, alignSelf: 'flex-end' }}>
+                                                <View style={{ flexDirection: 'row', alignSelf: 'flex-end', flex: 1 }}>
+                                                    <Icon
+                                                        name='favorite' size={24} color="#34343a" style={{ marginLeft: 5 }}
+                                                    />
+                                                    <Icon
+                                                        name='grade' size={24} color="#34343a" style={{ marginLeft: 5 }}
+                                                    />
+                                                </View>
+                                                <View style={{ flex: 1 }}></View>
+                                                <View style={{ flex: 1, alignSelf: 'flex-end' }}>
+                                                    <Button
+                                                        textStyle={{ fontSize: 15 }}
+                                                        iconRight
+                                                        backgroundColor='#705697'
+                                                        fontFamily='IRANSans_UltraLight'
+                                                        buttonStyle={{
+                                                            height: 15, width: 100,
+                                                            borderRadius: 8
+                                                        }}
+                                                        title='ادامه مطلب'
+                                                    />
+                                                </View>
+                                            </Row>
+                                        </Grid>
+                                    </Card>
+                                    <View style={{ flex: 1 }}></View>
+                                </Animatable.View>
+                            )
+                        })}
                     </View>
                 </ScrollView>
             </View>
